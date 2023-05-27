8bc698699af1ac1d956c8dad49f9c529ec8231f7
Aleksey <al.lyapin@bk.py>
2023-05-27 14:48:49 +0300

    v1.2

---
+++ backend/foodgram/urls.py
@@ -0,0 +1,14 @@
+import debug_toolbar
+from django.conf import settings
+from django.contrib import admin
+from django.urls import include, path
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls', namespace='api')),
+]
+if settings.DEBUG:
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls), name='debug_toolbar')
+    ]
