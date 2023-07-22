5ac08a9b5879de35b4f5b2a569f5feb304bd610f
AlexeyLyap <al.lyapin@bk.ru>
2023-07-22 12:36:27 +0300

    отладка

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     path('api/auth/', include('djoser.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL,
+#                           document_root=settings.MEDIA_ROOT)
